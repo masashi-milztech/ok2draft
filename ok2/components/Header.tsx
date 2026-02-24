295ee405d643cac8ca3b38236870d4d1b44be87a
masashi-milztech <masashi@milz.tech>
2026-02-24 12:40:25 +0900

    Update Header.tsx

--- ok2/components/Header.tsx
+++ ok2/components/Header.tsx
@@ -16,7 +16,7 @@ export const Header: React.FC = () => {
 
   const navLinks = [
     { name: 'PHILOSOPHY', href: '#philosophy' },
-    { name: 'WORKS', href: '#works' },
+    { name: 'SERVICE', href: '#works' },
     { name: 'COMPANY', href: '#company' },
     { name: 'CONTACT', href: '#contact' },
   ];
